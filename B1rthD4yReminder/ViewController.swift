07ea5e90af5ba9caf2fba4344d5e1f5e079ce6ff
9OSU1G02 <huyquocnguyen1902@gmail.com>
2020-12-10 13:09:48 +0700

    Initial Commit

---
+++ B1rthD4yReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  B1rthD4yReminder
+//
+//  Created by Nguyen Quoc Huy on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
